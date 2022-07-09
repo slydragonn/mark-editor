314f61063d233a0be59b3fc0955e75a6026347ed
slydragonn <alogo18m@gmail.com>
2022-07-08 19:19:58 -0500

    validate request method

--- pages/api/get-file.ts
+++ pages/api/get-file.ts
@@ -16,10 +16,13 @@ const identifyCode = (type:string, code:string) => {
 }
 
 export default async function handler(req:any, res:any) {
-  const {code, filename} = JSON.parse(req.body)
-  const type = req.headers['content-type']
-  res.setHeader('Content-Type', `${type}; charset=utf-8`)
-  res.setHeader('Content-Disposition', `attachment; filename=${filename}`)
-  res.status(200).write(identifyCode(type, code))
-  res.end()
+  if (req.method === "POST") {
+    const {code, filename} = JSON.parse(req.body)
+    const type = req.headers['content-type']
+    res.setHeader('Content-Type', `${type}; charset=utf-8`)
+    res.setHeader('Content-Disposition', `attachment; filename=${filename}`)
+    res.status(200).write(identifyCode(type, code))
+    return res.end()
+  }
+  return res.status(404).end()
 }
