13a314a27df0eb642624301e79f55c555b5e967f
slydragonn <alogo18m@gmail.com>
2022-07-14 22:58:07 -0500

    main tests for the editor

--- cypress/e2e/editor.cy.js
+++ cypress/e2e/editor.cy.js
@@ -1,4 +1,15 @@
-describe('editor renders correctly', () => {
+describe('editor page', () => {
+  const text = `
+## Sub title
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+
+~~~js
+function add(a, b) {
+  return a + b
+}
+~~~
+  `
+
   beforeEach(() => {
     cy.visit('/editor')
   })
@@ -9,14 +20,14 @@ describe('editor renders correctly', () => {
     cy.get('[data-cy="html-view"]').contains('h1', 'Hello World!')
   })
 
-  describe.only('navbar works', () => {
+  describe('navbar works', () => {
 
     it('home button redirects correctly', () => {
       cy.get('nav div a').click()
       cy.url().should('eq', 'http://localhost:3000/')
     })
 
-    it.only('menu is displayed', () => {
+    it('menu is displayed', () => {
       cy.get('nav ul').should('not.be.visible')
       cy.get('nav button').click()
       cy.get('nav ul').should('be.visible')
@@ -37,6 +48,23 @@ describe('editor renders correctly', () => {
           expect(value).to.eq('<h1 id="hello-world">Hello World!</h1>')
         })
       })
+    })
+  })
+  describe('editor works', () => {
+    it('code converts correctly', () => {
+      cy.get('[data-cy="markdown-editor"]').type(text)
+      cy.get('[data-cy="html-view"]').contains('h1', 'Hello World!')
+      cy.get('[data-cy="html-view"]').contains('h2', 'Sub title')
+      cy.get('[data-cy="html-view"]').contains('p', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
+      cy.get('[data-cy="html-view"]').contains('pre', 'function add(a, b) {')
+    })
+
+    it('last value is correct', () => {
+      cy.get('[data-cy="markdown-editor"]').type(text)
+      cy.reload()
+      cy.get('[data-cy="markdown-editor"]').contains('Hello World!')
+      cy.get('[data-cy="markdown-editor"]').contains('Sub title')
+      cy.get('[data-cy="markdown-editor"]').contains('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
     })
   })
 })
